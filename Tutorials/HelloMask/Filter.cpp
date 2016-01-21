e157055a86cc9eba92097d0a639c207bf3006d07
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-01-21 17:50:44 -0400

    Added sobol

--- Tutorials/HelloMask/Filter.cpp
+++ Tutorials/HelloMask/Filter.cpp
@@ -9,7 +9,7 @@ using namespace std;
 using namespace cv;
 
 // The tutorial shows how to manually do this, we later use filter2D
-void Sharpen(const Mat& inputImage, Mat& result){
+void sharpen(const Mat& inputImage, Mat& result){
     CV_Assert(inputImage.depth() == CV_8U);  // accept only uchar images
 
     // Create an output image the same size and type as input, save the number of channels
@@ -39,14 +39,14 @@ void Sharpen(const Mat& inputImage, Mat& result){
 
 int main(int argc, char const *argv[]){
     if (argc != 2){
-        cout << "Usage: Filter.o <Image_Path>" << endl;
+        cout << "Usage: " << argv[0] << " <Image_Path>" << endl;
         return -1;
     }
 
+    // Load & display original image
     string imageName = argv[1];
 
-    Mat image;
-    image = imread(imageName, 1);
+    Mat image = imread(imageName, 1);
 
     if (!image.data){
         cout << "No image data" << endl;
@@ -56,66 +56,98 @@ int main(int argc, char const *argv[]){
     namedWindow(imageName, WINDOW_AUTOSIZE);
     imshow(imageName, image);
 
-    // Manually Sharpened
-    Mat sharpenedImage;
-    Sharpen(image, sharpenedImage);
 
-    namedWindow("Sharpened", WINDOW_AUTOSIZE);
-    imshow("Sharpened", sharpenedImage);
+    // Define an output image and our kernel for the following spatial filtering
+    Mat output;
+    Mat kernel;
+
+
+    // Manually Sharpened
+    // Mat output;
+    // sharpen(image, output);
+    // namedWindow("Sharpened manually", WINDOW_AUTOSIZE);
+    // imshow("Sharpened manually", output);
+
 
     // Sharpened
-    Mat kernel = (Mat_<char>(3,3) <<  0, -1,  0,
-                                     -1,  5, -1,
-                                      0, -1,  0 );
-    Mat sharpenedByKernel;
-    filter2D(image, sharpenedByKernel, image.depth(), kernel);
+    kernel = (Mat_<char>(3,3) <<  0, -1,  0,
+                                 -1,  5, -1,
+                                  0, -1,  0 );
+    filter2D(image, output, image.depth(), kernel);
+    namedWindow("Sharpened", WINDOW_AUTOSIZE);
+    imshow("Sharpened", output);
 
-    namedWindow("Sharpened by filter2D", WINDOW_AUTOSIZE);
-    imshow("Sharpened by filter2D", sharpenedByKernel);
 
     // Averaged
     kernel = (Mat_<float>(3,3) <<  1, 1, 1,
                                    1, 1, 1,
                                    1, 1, 1 ) / 9.0;
-
-    Mat average;
-    filter2D(image, average, image.depth(), kernel);
-
+    filter2D(image, output, image.depth(), kernel);
     namedWindow("Average", WINDOW_AUTOSIZE);
-    imshow("Average", average);
+    imshow("Average", output);
+
 
     // Weighted Average
     kernel = (Mat_<float>(3,3) <<  1, 2, 1,
                                    2, 4, 2,
                                    1, 2, 1 ) / 16.0;
-
-    Mat weightedAverage;
-    filter2D(image, weightedAverage, image.depth(), kernel);
-
+    filter2D(image, output, image.depth(), kernel);
     namedWindow("Weighted Average", WINDOW_AUTOSIZE);
-    imshow("Weighted Average", weightedAverage);
+    imshow("Weighted Average", output);
+
 
     // Laplacian no diagonal
     kernel = (Mat_<float>(3,3) <<  0,  1, 0,
                                    1, -4, 1,
                                    0,  1, 0 );
-
-    Mat laplacianNoD;
-    filter2D(image, laplacianNoD, image.depth(), kernel);
-
+    filter2D(image, output, image.depth(), kernel);
     namedWindow("Laplacian no Diagonal", WINDOW_AUTOSIZE);
-    imshow("Laplacian no Diagonal", laplacianNoD);
+    imshow("Laplacian no Diagonal", output);
+
 
     // Laplacian with diagonal
     kernel = (Mat_<float>(3,3) <<  1,  1, 1,
                                    1, -8, 1,
                                    1,  1, 1 );
-
-    Mat laplacianWithD;
-    filter2D(image, laplacianWithD, image.depth(), kernel);
-
+    filter2D(image, output, image.depth(), kernel);
     namedWindow("Laplacian with Diagonal", WINDOW_AUTOSIZE);
-    imshow("Laplacian with Diagonal", laplacianWithD);
+    imshow("Laplacian with Diagonal", output);
+
+
+    // Sobol x no div
+    kernel = (Mat_<float>(3,3) << -1, 0, 1,
+                                  -2, 0, 2,
+                                  -1, 0, 1 );
+    filter2D(image, output, image.depth(), kernel);
+    namedWindow("Sobol x", WINDOW_AUTOSIZE);
+    imshow("Sobol x", output);
+
+
+    // Sobol x with div
+    kernel = (Mat_<float>(3,3) << -1, 0, 1,
+                                  -2, 0, 2,
+                                  -1, 0, 1 ) / 8.0;
+    filter2D(image, output, image.depth(), kernel);
+    namedWindow("Sobol x with div", WINDOW_AUTOSIZE);
+    imshow("Sobol x with div", output);
+
+
+    // Sobol y no div
+    kernel = (Mat_<float>(3,3) <<  1,  2,  1,
+                                   0,  0,  0,
+                                  -1, -2, -1 );
+    filter2D(image, output, image.depth(), kernel);
+    namedWindow("Sobol y", WINDOW_AUTOSIZE);
+    imshow("Sobol y", output);
+
+
+    // Sobol x with div
+    kernel = (Mat_<float>(3,3) <<  1,  2,  1,
+                                   0,  0,  0,
+                                  -1, -2, -1 ) / 8.0;
+    filter2D(image, output, image.depth(), kernel);
+    namedWindow("Sobol y with div", WINDOW_AUTOSIZE);
+    imshow("Sobol y with div", output);
 
     waitKey(0);
     return 0;
